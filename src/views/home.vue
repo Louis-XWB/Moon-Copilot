efd142d3083084d441ad3143ab9923b7ff3e42ac
wab <xuwab@outlook.com>
2023-06-30 21:42:53 +0800

    feat: edit tips

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,9 +3,9 @@
     <div
       class="flex flex-nowrap fixed w-full items-baseline top-0 px-6 py-4 bg-gray-100"
     >
-      <div class="text-2xl font-bold">ChatGPT</div>
+      <div class="text-2xl font-bold">Copilot</div>
       <div class="ml-4 text-sm text-gray-500">
-        基于 OpenAI 的 ChatGPT 自然语言模型人工智能对话
+        hack-quests 自然语言模型人工智能对话
       </div>
       <div
         class="ml-auto px-3 py-2 text-sm cursor-pointer hover:bg-white rounded-md"
